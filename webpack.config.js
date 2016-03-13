bf7736a98570651198cb260231aa6ccd3a69153d
Daniel Rector <rector.danielk@gmail.com>
2016-03-13 15:28:08 -0400

    added code to config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,12 @@
-code here
+module.exports = {
+    entry: "./entry.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    }
+};
